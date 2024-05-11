6150d6cf0c07d28157b3f74f2b281497263a3679
Fabian5150 <fabian.strauch.04@web.de>
2024-05-11 17:42:09 +0200

    generate random routes

--- src/main/java/com/example/DistributionCenter.java
+++ src/main/java/com/example/DistributionCenter.java
@@ -4,6 +4,48 @@ import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class DistributionCenter extends AbstractBehavior<DistributionCenter.Message>{
+    private ActorRef[] custArr;
+    private ArrayList<Packet> stockRoom = new ArrayList<>();
+
     public interface Message {};
+
+    private DistributionCenter(ActorContext<Message> context, ActorRef[] custArr) {
+        super(context);
+        //context.spawn(DeliveryCar.create(createRoute()), "car1");
+        //context.spawn(DeliveryCar.create(createRoute()), "car2");
+        //context.spawn(DeliveryCar.create(createRoute()), "car3");
+        //context.spawn(DeliveryCar.create(createRoute()), "car4");
+        this.custArr = custArr;
+        createRoute();
+        createRoute();
+        createRoute();
+    }
+
+    public static Behavior<DistributionCenter.Message> create(ActorRef[] custArr) {
+        return Behaviors.setup(context -> new DistributionCenter(context, custArr));
+    }
+
+    private ArrayList<ActorRef> createRoute(){
+        ArrayList<ActorRef> route = new ArrayList<>(4);
+
+        route.addAll(Arrays.asList(custArr));
+        Collections.shuffle(route);
+
+        getContext().getLog().info(route.get(0).toString());
+        getContext().getLog().info(route.get(3).toString());
+
+        return route;
+    }
+
+    @Override
+    public Receive<DistributionCenter.Message> createReceive() {
+        return newReceiveBuilder()
+                //.onMessage()
+                .build();
+    }
 }
